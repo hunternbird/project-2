90f1eabf85d3b3e32d875c341b7250eab2fde801
Hunter Bird <hnbird115@gmail.com>
2024-04-18 14:59:18 -0600

    finishing touches

--- products.js
+++ products.js
@@ -1,5 +1,5 @@
-const backgroundURLS = {
-    Vinyl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-png%2Fvinyl-record&psig=AOvVaw1u5egSueh8h0udUPX7Po6I&ust=1713295507961000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJi2u7P5xIUDFQAAAAAdAAAAABAE",
+const albumImages = {
+    Vinyl: "https://media.istockphoto.com/id/481475560/vector/vinyl-record-template.jpg?s=612x612&w=0&k=20&c=fZgBryspxNnRn8qMa1mEquff_T6wENAY1HXMtNEMyh4=",
 
 };
 
@@ -14,7 +14,7 @@ fetch(
 
 
 function handleEverything(musicData) {
-    console.log(musicData["Pop"]["Justin Timberlake"]["albums"]["0"]["name"]);
+    // console.log(musicData["Pop"]["Justin Timberlake"]["albums"]["0"]["name"]);
     // let artistData = printArtist(musicData);
     let artistData = Object.keys(musicData["Rock"]);
     makeArtistList(artistData, musicData);
@@ -26,11 +26,6 @@ function handleEverything(musicData) {
     // makeProductTable(albumData);
 }
 
-
-// function printArtist(musicData) {
-//     let artistData = musicData["Country"];
-//     return [artistData]
-// }
 
 
 
@@ -45,8 +40,12 @@ function makeArtistList(artistData, musicData) {
         // leftColumnList.innerHTML = artist;
         // artistNames.appendChild(leftColumnList);
         let leftColumnList = document.createElement('li');
+        leftColumnList.classList.add("artist-list-items");
+
         let artistLink = document.createElement('a');
-        artistLink.href = "#"; // Set the actual link href if you have it
+        artistLink.classList.add("artist-links");
+
+        artistLink.href = "#"; 
         artistLink.textContent = artist;
         artistLink.addEventListener('click', () => {
             displayArtistAlbums(artist, musicData);
@@ -59,16 +58,118 @@ function makeArtistList(artistData, musicData) {
 
 
 
+// function displayArtistAlbums(artist, musicData) {
+//     let albumData = musicData["Rock"][artist]["albums"];
+//     let productNames = document.getElementById("table-content");
+//     productNames.innerHTML = "";
+
+
+//     albumData.forEach(album => {
+//         // console.log(product);
+//         let rightColumnList = document.createElement('li');
+//         rightColumnList.innerHTML = album.name;
+//         productNames.appendChild(rightColumnList);
+//     });
+
+// }
+
 function displayArtistAlbums(artist, musicData) {
     let albumData = musicData["Rock"][artist]["albums"];
-    let productNames = document.getElementById("table-content");
-    productNames.innerHTML = "";
+    let tableContent = document.getElementById("table-content");
+    tableContent.innerHTML = ""; 
 
+
+    let table = document.createElement("table");
+    table.style.border = "1px solid black"; 
+
+    
+    let head = document.createElement("tr")
+    let tableheader = document.createElement("th")
+    tableheader.classList.add("table-header-name");
+    
+    tableheader.innerHTML = artist + " Albums:"
+    head.appendChild(tableheader)
+    table.appendChild(head)
+    
+    // makes only one long row
+    // let row = document.createElement("tr");
+    
+    
     albumData.forEach(album => {
-        // console.log(product);
-        let rightColumnList = document.createElement('li');
-        rightColumnList.innerHTML = album.name;
-        productNames.appendChild(rightColumnList);
+        
+
+
+
+        // makes a row for each album
+        let row = document.createElement("tr");
+
+        let imageCell = document.createElement("img");
+        imageCell.classList.add("record-image");
+        imageCell.src = albumImages["Vinyl"];
+        // imageCell.style.width = "5rem";
+        
+
+
+        let cell = document.createElement("td");
+        cell.textContent = "Album: " + album.name;
+        cell.style.border = "1px solid black";
+
+        let stockCell = document.createElement("td");
+        stockCell.textContent = "In Stock: " + album.in_stock + " records";
+        stockCell.style.border = "1px solid black";
+
+        let button = document.createElement("button");
+        button.classList.add("buy-now-button");
+        button.textContent = "See track list";
+
+        button.addEventListener('click', () => {
+            displayTrackRecords(album);
+        });
+
+
+        cell.appendChild(imageCell);
+        stockCell.appendChild(button);
+        row.appendChild(cell);
+        row.appendChild(stockCell);
+
+        table.appendChild(row);
     });
+
+    tableContent.appendChild(table);
 }
 
+
+
+function displayTrackRecords(album) {
+    // Clear previous content
+    let tableContent = document.getElementById("table-content");
+    tableContent.innerHTML = "";
+
+    // Create a table to display track records
+    let table = document.createElement("table");
+    table.style.border = "1px solid black";
+
+    // Create table header
+    let headerRow = document.createElement("tr");
+    let trackHeader = document.createElement("th");
+    trackHeader.textContent = "Track";
+    let durationHeader = document.createElement("th");
+    durationHeader.textContent = "Duration";
+    headerRow.appendChild(trackHeader);
+    headerRow.appendChild(durationHeader);
+    table.appendChild(headerRow);
+
+    // Loop through each track in the album
+    album.tracks.forEach(track => {
+        let row = document.createElement("tr");
+        let trackCell = document.createElement("td");
+        trackCell.textContent = track.name;
+        let durationCell = document.createElement("td");
+        durationCell.textContent = track.duration + " seconds";
+        row.appendChild(trackCell);
+        row.appendChild(durationCell);
+        table.appendChild(row);
+    });
+
+    tableContent.appendChild(table);
+}
